dd5876303c180e94dee0d9caf01004eea2c32b05
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-06-04 10:18:16 +0200

    Update test

--- catalog-metadata-rest/src/test/java/no/nb/microservices/catalogmetadata/service/CassandraServiceTest.java
+++ catalog-metadata-rest/src/test/java/no/nb/microservices/catalogmetadata/service/CassandraServiceTest.java
@@ -18,6 +18,7 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(EasyMockRunner.class)
 public class CassandraServiceTest {
@@ -43,10 +44,12 @@ public class CassandraServiceTest {
         File modsFile = new File(Paths.get(getClass().getResource("mods1.xml").toURI()).toString());
         String modsString = FileUtils.readFileToString(modsFile);
         expect(cassandraRepository.getModsString("c06c5cbe2f82113e7b4757dbb14f8676")).andReturn(modsString);
+        expect(cassandraRepository.getModsString("bogusid")).andReturn(null);
         replay(cassandraRepository);
 
         ModsType mods = cassandraService.getMods("c06c5cbe2f82113e7b4757dbb14f8676");
         assertNotNull(mods);
+        assertNull(cassandraService.getMods("bogusid"));
         verify(cassandraRepository);
     }
 
@@ -55,10 +58,12 @@ public class CassandraServiceTest {
         File modsFile = new File(Paths.get(getClass().getResource("mods1.xml").toURI()).toString());
         String modsString = FileUtils.readFileToString(modsFile);
         expect(cassandraRepository.getModsString("c06c5cbe2f82113e7b4757dbb14f8676")).andReturn(modsString);
+        expect(cassandraRepository.getModsString("bogusid")).andReturn(null);
         replay(cassandraRepository);
 
         RecordType marc = cassandraService.getMarcxml("c06c5cbe2f82113e7b4757dbb14f8676");
         assertNotNull(marc);
+        assertNull(cassandraService.getMods("bogusid"));
         verify(cassandraRepository);
     }
 }
