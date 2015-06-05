564dbf8f6c58f7d6d05d294678f63266cfc9950d
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-06-05 10:23:32 +0200

    Removed unused constructors

--- catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/exception/CassandraSessionException.java
+++ catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/exception/CassandraSessionException.java
@@ -2,22 +2,7 @@ package no.nb.microservices.catalogmetadata.exception;
 
 public class CassandraSessionException extends RuntimeException {
 
-    public CassandraSessionException() {
-    }
-
-    public CassandraSessionException(String message) {
-        super(message);
-    }
-
     public CassandraSessionException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public CassandraSessionException(Throwable cause) {
-        super(cause);
-    }
-
-    public CassandraSessionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
