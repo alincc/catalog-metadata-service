5cf63cc7b1ad9234a71f64f6dda36015b1513895
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-06-09 13:39:59 +0200

    Removed unused imports

--- catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/Application.java
+++ catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/Application.java
@@ -1,18 +1,12 @@
 package no.nb.microservices.catalogmetadata;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class Application {
 
