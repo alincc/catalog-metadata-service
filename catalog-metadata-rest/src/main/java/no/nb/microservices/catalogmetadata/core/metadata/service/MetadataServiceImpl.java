d1bc74687b4909bbaa3db4c175719601878ef2dc
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-06-12 12:23:16 +0200

    Removed unused imports

--- catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/core/metadata/service/MetadataServiceImpl.java
+++ catalog-metadata-rest/src/main/java/no/nb/microservices/catalogmetadata/core/metadata/service/MetadataServiceImpl.java
@@ -1,20 +1,16 @@
 package no.nb.microservices.catalogmetadata.core.metadata.service;
 
-import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.transform.stream.StreamSource;
 
-import com.datastax.driver.core.utils.Bytes;
 import loc.gov.marc.RecordType;
 import no.nb.microservices.catalogmetadata.core.metadata.repository.IMetadataRepository;
 import no.nb.microservices.catalogmetadata.core.transform.service.ITransformerService;
 import no.nb.microservices.catalogmetadata.core.transform.service.TransformerServiceImpl;
-import no.nb.microservices.catalogmetadata.domain.Model;
 import no.nb.microservices.catalogmetadata.exception.FieldNotFoundException;
 import no.nb.microservices.catalogmetadata.exception.FieldsParserException;
 import no.nb.microservices.catalogmetadata.exception.ModsNotFoundException;
@@ -24,7 +20,6 @@ import no.nb.microservices.catalogmetadata.model.fields.Fields;
 import no.nb.microservices.catalogmetadata.model.mods.v3.Mods;
 import no.nb.microservices.catalogmetadata.model.struct.StructMap;
 
-import no.nb.microservices.catalogmetadata.utils.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
@@ -81,10 +76,15 @@ public class MetadataServiceImpl implements IMetadataService {
         ObjectMapper mapper = new ObjectMapper();
         Fields fields;
         try {
-            List<Field> fieldlist = mapper.readValue(map.get("fields"), new TypeReference<List<Field>>(){});
+            List<Field> fieldlist = mapper.readValue(map.get("fields"), new TypeReference<List<Field>>(){
+            });
+
             fields = populateField(fieldlist);
-            fields.setContentClasses(mapper.readValue(map.get("contentClasses"), new TypeReference<List<String>>(){}));
-            fields.setMetadataClasses(mapper.readValue(map.get("metadataClasses"), new TypeReference<List<String>>(){}));
+            fields.setContentClasses(mapper.readValue(map.get("contentClasses"), new TypeReference<List<String>>(){
+            }));
+
+            fields.setMetadataClasses(mapper.readValue(map.get("metadataClasses"), new TypeReference<List<String>>(){
+            }));
         } catch (Exception ex) {
             throw new FieldsParserException("Error parsing " + id, ex);
         }
