565257d00fd802c70579485be741d51300498367
raymondkarstensen <raymond.karstensen@nb.no>
2015-09-14 15:29:59 +0200

    BT-3305 - catalog-item-service: recordinfo/created - Mapping mot recordCreationDate

--- catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/RecordInfo.java
+++ catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/RecordInfo.java
@@ -2,12 +2,14 @@ package no.nb.microservices.catalogmetadata.model.mods.v3;
 
 import javax.xml.bind.annotation.XmlElement;
 import java.io.Serializable;
+import java.util.Date;
 
 public class RecordInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String recordContentSource;
     private RecordIdentifier recordIdentifier;
+    private Date creationDate;
 
     @XmlElement(name = "recordContentSource", namespace = "http://www.loc.gov/mods/v3")
     public String getRecordContentSource() {
@@ -25,5 +27,14 @@ public class RecordInfo implements Serializable {
 
     public void setRecordIdentifier(RecordIdentifier recordIdentifier) {
         this.recordIdentifier = recordIdentifier;
+    }
+
+    @XmlElement(name = "recordCreationDate", namespace = "http://www.loc.gov/mods/v3")
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
     }
 }
