6f3fdb50a833918140899591cfc298dd825dadd2
ronnymikalsen <ronny.mikalsen@nb.no>
2015-10-05 15:14:53 +0200

    Added Authority to language

--- catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/LanguageTerm.java
+++ catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/LanguageTerm.java
@@ -1,15 +1,14 @@
 package no.nb.microservices.catalogmetadata.model.mods.v3;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 import java.io.Serializable;
 
-/**
- * Created by rolfm on 01.10.15.
- */
 public class LanguageTerm implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String value;
+    private String authority;
 
     @XmlValue
     public String getValue() {
@@ -18,6 +17,15 @@ public class LanguageTerm implements Serializable {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @XmlAttribute
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
     }
 
 
