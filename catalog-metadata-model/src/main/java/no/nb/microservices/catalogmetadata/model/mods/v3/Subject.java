ccdf7692a06b7a07ac3669fab8c566e6a2edde98
ronnymikalsen <ronny.mikalsen@nb.no>
2015-10-27 20:40:15 +0100

    Added names attribute

--- catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/Subject.java
+++ catalog-metadata-model/src/main/java/no/nb/microservices/catalogmetadata/model/mods/v3/Subject.java
@@ -1,19 +1,30 @@
 package no.nb.microservices.catalogmetadata.model.mods.v3;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import java.io.Serializable;
 import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 
 public class Subject implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private List<Name> names;
     private List<Topic> topics;
     private List<String> genres;
     private List<Geographic> geographic;
     private Cartographics cartographics;
     private String authority;
 
+    @XmlElement(name = "names", namespace = "http://www.loc.gov/mods/v3")
+	public List<Name> getNames() {
+		return names;
+	}
+
+	public void setNames(List<Name> names) {
+		this.names = names;
+	}
+    
     @XmlElement(name = "topic", namespace = "http://www.loc.gov/mods/v3")
     public List<Topic> getTopic() {
         return topics;
@@ -58,4 +69,5 @@ public class Subject implements Serializable {
     public void setAuthority(String authority) {
         this.authority = authority;
     }
+
 }
